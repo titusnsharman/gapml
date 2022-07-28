885244f08c944e5eb871e232259b307f1775a04b
titus.sharman <titus.sharman@gmail.com>
2022-07-28 15:57:05 -0700

    Added mypaths directy

---
+++ gestalt/constant_paths.py
@@ -0,0 +1,3 @@
+MIX_PATH = "~/my_path/mix" # download from phylip website
+BHV_PATH = "~/my_path/gtp.jar" # download from http://comet.lehman.cuny.edu/owen/code.html
+RSPR_PATH = "~/my_path/rspr" # download from https://github.com/cwhidden/rspr
